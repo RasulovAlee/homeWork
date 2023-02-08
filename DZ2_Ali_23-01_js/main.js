1e9660825deb3e4e924ecdb7b121906f432fe5a3
RasulovAlee <122157164+RasulovAlee@users.noreply.github.com>
2023-02-08 12:25:37 +0600

    D2_js

---
+++ DZ2_Ali_23-01_js/main.js
@@ -0,0 +1,17 @@
+// for (var i = 20; i > 0; i--){
+//     console.log(i)
+// }
+//
+// var colors = prompt('Choose color')
+// if (colors ==='red') {
+//     alert('Stop!')
+// }else if (colors ==='yellow') {
+//    alert('Wait!')
+// }else if (colors === 'green') {
+//     alert('Go')
+// }else {
+//     alert('Choose color!')
+// }
+num = [1, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+console.log(num[0], num[1], num[5], num[10])
+
